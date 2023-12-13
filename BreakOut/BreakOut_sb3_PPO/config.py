d3d8267303285f402eccea130961696dccdfc0cc
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-12-13 08:44:31 +0100

    Add files via upload

--- BreakOut/BreakOut_sb3_PPO/config.py
+++ BreakOut/BreakOut_sb3_PPO/config.py
@@ -6,16 +6,16 @@ import torch
 '''FILE TO STORE ALL THE CONFIGURATION VARIABLES'''
 
 #pretrained is a boolean that indicates if a pretrained model will be loaded
-pretrained = True # Set to True if you want to load a pretrained model
+pretrained = False # Set to True if you want to load a pretrained model
 
 #check_freq is the frequency at which the callback is called, in this case, the callback is called every 2000 timesteps
 check_freq = 2000
 
 #save_path is the path where the best model will be saved
-save_path = "/home/ndelafuente/Downloads/Learn2Earn_RL/BreakOut/BreakOut_sb3_PPO2/breakout_ppo2_1M_save_path"
+save_path = "/home/ndelafuente/Downloads/Learn2Earn_RL/BreakOut/BreakOut_sb3_PPO/breakout_ppo_1M_save_path"
 
 #log_dir is the path where the logs will be saved
-log_dir = "/home/ndelafuente/Downloads/Learn2Earn_RL/BreakOut/BreakOut_sb3_PPO2/log_dir"
+log_dir = "/home/ndelafuente/Downloads/Learn2Earn_RL/BreakOut/BreakOut_sb3_PPO/log_dir"
 
 
 '''
@@ -25,7 +25,7 @@ Hyperparameters of the model {learning_rate, gamma, device, n_steps, gae_lambda,
 policy = "CnnPolicy"
 
 #learning_rate is the learning rate of the model
-learning_rate = 2.5e-3
+learning_rate =5e-4  #first trial: 5e-4   #second trial: 1e-4  #third trial: 1e-3  #fourth trial: 5e-5 #fifth trial: 5e-5 gamma = 0.90 #sixth trial: 1e-4 gamma = 0.90 #seventh trial: 5e-4 gamma = 0.90
 
 #gamma is the discount factor
 gamma = 0.99
@@ -103,7 +103,7 @@ _init_setup_model = True
 #Take into account that the number of timesteps is not the number of episodes, in a game like breakout, the agent takes an action every frame,
 # then the number of timesteps is the number of frames, which is the number of frames in 1 game multiplied by the number of games played.
 #The average number of frames in 1 game is 1000, so 1e7 timesteps is 1000 games more or less.
-total_timesteps = int(7e6)
+total_timesteps = int(3e7)
 
 #log_interval is the number of timesteps between each log, in this case, the training process will be logged every 100 timesteps.
 log_interval = 100
@@ -113,8 +113,8 @@ Saved model path
 '''
 
 #for the path to be shorter just put "./a2c_Breakout_1M.zip" instead of the full path
-saved_model_path = "./PPO_Breakout_7M.zip"
-unzip_file_path =  "./PPO_Breakout_7M_unzipped"
+saved_model_path = "./PPO_Breakout_30M_lr_5e-4_gamma_90.zip"
+unzip_file_path =  "./PPO_Breakout_30M_lr_5e-4_gamma_90_unzipped"
 
 '''
 Environment variables
@@ -128,14 +128,14 @@ n_envs = 4
 Wandb configuration
 '''
 # project is the name of the project in wandb
-project_train = "breakout-PPO2"
+project_train = "BREAKOUT_SB3_BENCHMARK"
 project_test = "breakout-PPO2-test"
 
 #entity is the name of the team in wandb
 entity = "ai42"
 
 #name is the name of the run in wandb
-name_train = "PPO2_breakout{}".format(get_latest_run_id("/home/ndelafuente/Downloads/Learn2Earn_RL/BreakOut/BreakOut_sb3_PPO2/log_dir/PPO2_breakout/"))
+name_train = "PPO_breakout_lr_5e-4_gamma_90"
 name_test = "PPO2_breakout_test"
 #notes is a description of the run
 notes = "PPO2_breakout with parameters: {}".format(locals()) #locals() returns a dictionary with all the local variables, in this case, all the variables in this file
